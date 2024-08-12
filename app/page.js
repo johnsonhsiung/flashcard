df112e6b4f327b5c752ce4182ffb2a9dcb8bdae3
Johnson Hsiung <johnson.hsiung@sjsu.edu>
2024-08-12 15:45:45 -0700

    add clerk and very basic UI

--- app/page.js
+++ app/page.js
@@ -1,7 +1,34 @@
 import Image from "next/image";
+import getStripe from "@/utils/get-stripe";
+import {SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
+import { Button, Toolbar, Typography, Container, AppBar } from "@mui/material";
+import Head from "next/head"
 
 
 export default function Home() {
-  return 
+    
+  return <Container maxwidth='100%'>
+    <Head>
+      <title>
+        Flashcard 
+      </title>
+      <meta name='descrption' content="Create flashcards from your text" />
+    </Head>
+    <AppBar position ='static'>
+      <Toolbar>
+        <Typography variant='h6' style= {{flexgrow: 1 }}>
+          Flashcard SaaS
+        </Typography>
+        <SignedOut>
+          <Button color='inherit'>Login</Button>
+          <Button color='inherit'>Sign Up</Button>
+        </SignedOut>
+        <SignedIn> 
+          <UserButton/> 
+        </SignedIn>
+      </Toolbar>
+    </AppBar>
+  </Container>
+
 
 }
