d1408f2a698a3f34d23ecfdf770134f51ad619db
GHGFaber <moises.fuentes904@gmail.com>
2024-08-13 00:00:34 -0700

    added Title and call to Action Button

--- app/page.js
+++ app/page.js
@@ -1,34 +1,47 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
-import {SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
-import { Button, Toolbar, Typography, Container, AppBar } from "@mui/material";
-import Head from "next/head"
-
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import {
+  Button,
+  Toolbar,
+  Typography,
+  Container,
+  AppBar,
+  Box,
+} from "@mui/material";
+import Head from "next/head";
 
 export default function Home() {
-    
-  return <Container maxwidth='100%'>
-    <Head>
-      <title>
-        Flashcard 
-      </title>
-      <meta name='descrption' content="Create flashcards from your text" />
-    </Head>
-    <AppBar position ='static'>
-      <Toolbar>
-        <Typography variant='h6' style= {{flexgrow: 1 }}>
-          Flashcard SaaS
+  return (
+    <Container maxWidth="lg">
+      <Head>
+        <title>Flashcard</title>
+        <meta name="descrption" content="Create flashcards from your text" />
+      </Head>
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            Flashcard SaaS
+          </Typography>
+          <SignedOut>
+            <Button color="inherit">Login</Button>
+            <Button color="inherit">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{ textAlign: "center", my: 4 }}>
+        <Typography variant="h2">Welcome to Flashcard SaaS</Typography>
+        <Typography variant="h5">
+          The easiest way to make flashcards from text
         </Typography>
-        <SignedOut>
-          <Button color='inherit'>Login</Button>
-          <Button color='inherit'>Sign Up</Button>
-        </SignedOut>
-        <SignedIn> 
-          <UserButton/> 
-        </SignedIn>
-      </Toolbar>
-    </AppBar>
-  </Container>
-
-
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+          Get Started
+        </Button>
+      </Box>
+    </Container>
+  );
 }
