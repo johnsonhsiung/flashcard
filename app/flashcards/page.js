fc6786e0ca66736914979cdee7e0ea1a30777b0a
GHGFaber <moises.fuentes904@gmail.com>
2024-08-13 18:12:49 -0700

    modified firebase to export db and added beginnings of flashcard page

---
+++ app/flashcards/page.js
@@ -0,0 +1,69 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/router";
+
+export default function Generate() {
+  const { isLoaded, isSignedin, user } = useUser();
+  const [flashcards, setFlashcards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    fetch("api/generate", {
+      method: "POST",
+      body: text,
+    })
+      .then((res) => res.json())
+      .then(data > setFlashcards(data));
+  };
+
+  const handleCardClick = (id) => {
+    setFlipped((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const saveFlashcards = async () => {
+    if (!name) {
+      alert("Please enter a name");
+      return;
+    }
+    const batch = writeBatch(db);
+    const userDocRef = doc(collection(db, "users"), user.id);
+    const docSnap = await getDoc(userDocRef);
+
+    if (docSnap.exists()) {
+      const collections = docSnap.data().flashcards || [];
+
+      if (collections.find((f) => f.name == name)) {
+        alert("Flashcard collection with the name already exists!");
+      } else {
+        collections.push(name);
+        batch.set(userDocRef, { flashcards: collections }, { merge: true });
+      }
+    } else {
+      batch.set(userDocRef, { flashcards: [{ name }] });
+    }
+
+    const colRef = collection(userDocRef, name);
+    flashcards.forEach((flashcard) => {
+      const cardDocRef = doc(colRef);
+      batch.set(cardDocRef, flashcard);
+    });
+
+    await batch.commit();
+    handleClose();
+    router.push("/flashcards");
+  };
+}
